acc4c45dcb7d5eeaac2a221656a73fa37111d8d8
daffaYuharshu <daffayuharshu@gmail.com>
2024-08-11 19:56:11 +0700

    remove unused variable

--- src/controllers/person-controller.js
+++ src/controllers/person-controller.js
@@ -4,7 +4,6 @@ const {
   addPerson,
   getAllPersonByOwner,
   getCountPersonByOwner,
-  userId,
   getAllPersonByOwnerHaveReports,
   getCountPersonByOwnerHaveReports,
   getAllPersonByOwnerHaveReportsFilteredByNIK,
