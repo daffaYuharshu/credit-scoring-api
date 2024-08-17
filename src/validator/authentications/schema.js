06531677c285db484545b5080ea0621981261213
daffaYuharshu <daffayuharshu@gmail.com>
2024-08-17 15:15:00 +0700

    add response validator indonesian

--- src/validator/authentications/schema.js
+++ src/validator/authentications/schema.js
@@ -4,21 +4,25 @@ const PostAuthenticationPayloadSchema = Joi.object({
   email: Joi.string().email().required().messages({
     "string.email": "Email harus berupa format email yang valid.",
     "any.required": "Email wajib diisi.",
+    "string.empty": "Email tidak boleh kosong.",
   }),
   password: Joi.string().required().messages({
-    "any.required": "Kata sandi wajib diisi.",
+    "any.required": "Password wajib diisi.",
+    "string.empty": "Password tidak boleh kosong.",
   }),
 });
 
 const PutAuthenticationPayloadSchema = Joi.object({
   refreshToken: Joi.string().required().messages({
     "any.required": "Refresh token wajib diisi.",
+    "string.empty": "Refresh token tidak boleh kosong.",
   }),
 });
 
 const DeleteAuthenticationPayloadSchema = Joi.object({
   refreshToken: Joi.string().required().messages({
     "any.required": "Refresh token wajib diisi.",
+    "string.empty": "Refresh token tidak boleh kosong.",
   }),
 });
 
--- src/validator/users/schema.js
+++ src/validator/users/schema.js
@@ -2,18 +2,22 @@ const Joi = require("joi");
 
 const UserPayloadSchema = Joi.object({
   username: Joi.string().required().messages({
-    "any.required": "Nama pengguna wajib diisi.",
+    "any.required": "Username wajib diisi.",
+    "string.empty": "Username tidak boleh kosong.",
   }),
   email: Joi.string().email().required().messages({
     "string.email": "Email harus berupa format email yang valid.",
     "any.required": "Email wajib diisi.",
+    "string.empty": "Email tidak boleh kosong.",
   }),
   password: Joi.string().required().messages({
-    "any.required": "Kata sandi wajib diisi.",
+    "any.required": "Password wajib diisi.",
+    "string.empty": "Password tidak boleh kosong.",
   }),
   role: Joi.string().valid("user", "admin").required().messages({
     "any.only": 'Role harus salah satu dari "user" atau "admin".',
-    "any.required": "Peran wajib diisi.",
+    "any.required": "Role wajib diisi.",
+    "string.empty": "Role tidak boleh kosong.",
   }),
 });
 
