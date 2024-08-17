4b8f7b0d589bf6917db22b1ad1cce915830df1ff
daffaYuharshu <daffayuharshu@gmail.com>
2024-08-17 15:22:41 +0700

    remove dot

--- src/validator/authentications/schema.js
+++ src/validator/authentications/schema.js
@@ -2,20 +2,20 @@ const Joi = require("joi");
 
 const PostAuthenticationPayloadSchema = Joi.object({
   email: Joi.string().email().required().messages({
-    "string.email": "Email harus berupa format email yang valid.",
-    "any.required": "Email wajib diisi.",
-    "string.empty": "Email tidak boleh kosong.",
+    "string.email": "Email harus berupa format email yang valid",
+    "any.required": "Email wajib diisi",
+    "string.empty": "Email tidak boleh kosong",
   }),
   password: Joi.string().required().messages({
-    "any.required": "Password wajib diisi.",
-    "string.empty": "Password tidak boleh kosong.",
+    "any.required": "Password wajib diisi",
+    "string.empty": "Password tidak boleh kosong",
   }),
 });
 
 const PutAuthenticationPayloadSchema = Joi.object({
   refreshToken: Joi.string().required().messages({
-    "any.required": "Refresh token wajib diisi.",
-    "string.empty": "Refresh token tidak boleh kosong.",
+    "any.required": "Refresh token wajib diisi",
+    "string.empty": "Refresh token tidak boleh kosong",
   }),
 });
 
--- src/validator/users/schema.js
+++ src/validator/users/schema.js
@@ -2,22 +2,22 @@ const Joi = require("joi");
 
 const UserPayloadSchema = Joi.object({
   username: Joi.string().required().messages({
-    "any.required": "Username wajib diisi.",
-    "string.empty": "Username tidak boleh kosong.",
+    "any.required": "Username wajib diisi",
+    "string.empty": "Username tidak boleh kosong",
   }),
   email: Joi.string().email().required().messages({
-    "string.email": "Email harus berupa format email yang valid.",
-    "any.required": "Email wajib diisi.",
-    "string.empty": "Email tidak boleh kosong.",
+    "string.email": "Email harus berupa format email yang valid",
+    "any.required": "Email wajib diisi",
+    "string.empty": "Email tidak boleh kosong",
   }),
   password: Joi.string().required().messages({
-    "any.required": "Password wajib diisi.",
-    "string.empty": "Password tidak boleh kosong.",
+    "any.required": "Password wajib diisi",
+    "string.empty": "Password tidak boleh kosong",
   }),
   role: Joi.string().valid("user", "admin").required().messages({
-    "any.only": 'Role harus salah satu dari "user" atau "admin".',
-    "any.required": "Role wajib diisi.",
-    "string.empty": "Role tidak boleh kosong.",
+    "any.only": 'Role harus salah satu dari "user" atau "admin"',
+    "any.required": "Role wajib diisi",
+    "string.empty": "Role tidak boleh kosong",
   }),
 });
 
